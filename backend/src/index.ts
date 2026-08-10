612a1c7223c7735e72edfb3a3e2e787c62158b03
Mayank Sahu <168402822+Deadshot-45@users.noreply.github.com>
2026-08-10 19:48:44 +0530

    chore: rename DockerFile to Dockerfile and fix default backend port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import { clerkMiddleware } from "@clerk/express";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 // Middleware
